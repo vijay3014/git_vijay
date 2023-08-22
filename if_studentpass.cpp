1804c1d882ac410e8629fd95f4ad925e2446ff79
vijay solanki <142559602+vijay3014@users.noreply.github.com>
2023-08-22 17:38:23 +0530

    Add files via upload

---
+++ if_studentpass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    int marks;
+
+   
+     cout << "Enter the student marks: ";
+     cin >> marks;
+
+    
+    if (marks >= 33)
+     {
+       cout << "The student is passed."; 
+     } else
+      {
+       cout << " the student is not passed.";
+     }
+
+    
+}
---
+++ inch_cm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ {
+   float inches, centimeters;
+
+    
+    cout << "Enter length in inches: ";
+    cin >> inches;
+
+  
+    centimeters = inches * 2.54;
+
+   cout << inches << " inches to " << centimeters << " centimeters.";
+
+    
+}
